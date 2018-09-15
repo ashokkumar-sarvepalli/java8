342c5f6c30e1245bbbc8c5155b2d00c79374e3e9
unknown <ashokkumar.sarvepalli@gmail.com>
2018-09-15 16:48:01 -0400

    java 8 map operations

--- src/learning/java8/empoyee/EmployeeHandler.java
+++ src/learning/java8/empoyee/EmployeeHandler.java
@@ -7,18 +7,18 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class EmployeeHandler {
-	
-	private Map<String,Employee>  employeeMap = new HashMap<String,Employee>();
-	private Map<String,Manager> managerMap = new HashMap<String,Manager>();
-	
+
+	private Map<String, Employee> employeeMap = new HashMap<String, Employee>();
+	private Map<String, Manager> managerMap = new HashMap<String, Manager>();
+
 	public void createEmployee(Employee employee) {
-		employeeMap.put(employee.getEmpId(),employee);
+		employeeMap.put(employee.getEmpId(), employee);
 		Manager manager = managerMap.get(employee.getMgrId());
 		// add the employee to the manager list
 		manager.getEmpList().add(employee);
-		
+
 	}
-	
+
 	public void removeEmployee(String empId) {
 		Employee employee = employeeMap.get(empId);
 		Manager manager = managerMap.get(employee.getMgrId());
@@ -27,11 +27,11 @@ public class EmployeeHandler {
 		// remove from employee
 		employeeMap.remove(empId);
 	}
-	
+
 	public void createManager(Manager manager) {
 		managerMap.put(manager.getMgrId(), manager);
 	}
-	
+
 	public void changeManager(String srcMgrId, String destMgrId) {
 		// first update the manager id of all employees.
 		Set<Employee> employeeSet = managerMap.get(srcMgrId).getEmpList();
@@ -43,51 +43,43 @@ public class EmployeeHandler {
 		Manager oldManager = managerMap.get(srcMgrId);
 		oldManager.getEmpList().clear();
 	}
-	
+
 	public Employee searchEmployee(String empId) {
 		return employeeMap.get(empId);
 	}
-	
-	public Set<Employee> displayAllEmployeesByManagerId(String managerId){
+
+	public Set<Employee> displayAllEmployeesByManagerId(String managerId) {
 		return managerMap.get(managerId).getEmpList();
 	}
-	
+
 	public Manager searchManager(String mgrId) {
 		return managerMap.get(mgrId);
 	}
-	
+
 	public Employee displayEmployeeWithHighestSalaryUnderGivenManager(String mgrId) {
 		return managerMap.get(mgrId).getEmpList().stream().max(Comparator.comparing(Employee::getSalary)).get();
 	}
-	
+
 	public Set<Employee> sortEmployeesWithSalaryForGivenManager(String mgrId) {
-		return managerMap.get(mgrId).getEmpList().stream().sorted(Comparator.comparing(Employee::getSalary)).collect(Collectors.toSet());
+		return managerMap.get(mgrId).getEmpList().stream().sorted(Comparator.comparing(Employee::getSalary))
+				.collect(Collectors.toSet());
 	}
-	
-	
-	public Map<String,Integer> countOfEmployeesUnderEachManager(){
-		Map<String,Integer> employeeUnderEachManagerCount = new HashMap<String,Integer>();
-		managerMap.forEach((id,manager)-> employeeUnderEachManagerCount.put(id,manager.getEmpList().size()));
+
+	public Map<String, Integer> countOfEmployeesUnderEachManager() {
+		Map<String, Integer> employeeUnderEachManagerCount = new HashMap<String, Integer>();
+		managerMap.forEach((id, manager) -> employeeUnderEachManagerCount.put(id, manager.getEmpList().size()));
 		return employeeUnderEachManagerCount;
 	}
-	
-	
-	public Map<String,Integer> countOfEmployeesUnderEachManagerGreaterThanSpecificSalary(double salary){
-		Map<String,Integer> employeeUnderEachManagerCount = new HashMap<String,Integer>();
-		//
+
+	public Map<String, Long> countOfEmployeesUnderEachManagerGreaterThanSpecificSalary(double salary) {
+		Map<String, Long> employeeUnderEachManagerCount = new HashMap<String, Long>();
+		managerMap.forEach((id, manager) -> employeeUnderEachManagerCount.put(id,
+				manager.getEmpList().stream().filter(emp -> emp.getSalary() > salary).count()));
 		return employeeUnderEachManagerCount;
 	}
-	
+
 	public Long countOfEmployeesWithSalaryGreaterThanGivenValue(double salary) {
 		return employeeMap.values().stream().filter(emp -> emp.getSalary() > salary).count();
 	}
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
